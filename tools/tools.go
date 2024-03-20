460fb6133657dd9372497793d71548b2ae26e33f
Cleyan Sampaio <auryannn@gmail.com>
2024-03-20 12:18:47 -0300

    Test

--- tools/tools.go
+++ tools/tools.go
@@ -1,11 +1,15 @@
 //go:build tools
 // +build tools
+
+//
+//nolint:go list
 package tools
 
 // Manage tool dependencies via go.mod.
 //
 // https://go.dev/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
 // https://github.com/golang/go/issues/25922
+//nolint:compiler
 import (
 	_ "github.com/client9/misspell/cmd/misspell"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
